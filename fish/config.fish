5b35e2a84eab9473d1d931affaccd742d047a8ae
Omar Mohamed <121226656+omarx69@users.noreply.github.com>
2025-08-21 09:04:11 +0300

    Update config.fish
    Making FZF trasnparent

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,12 @@ starship init fish | source
 
 # FZF colors (Catppuccin Mocha)
 set -Ux FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+--color=bg+:-1,bg:-1,spinner:#F5E0DC,hl:#F38BA8 \
 --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
 --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
 --color=selected-bg:#45475A \
---color=border:#313244,label:#CDD6F4"
+--color=border:#6C7086,label:#CDD6F4"
+
 
 # BAT theme
 set -Ux BAT_THEME "Catppuccin Mocha"
